49d46654393b6d7ebbda16db4c430093b77fbb59
Paweł Szydłowski` <p.szydlowski.work@gmail.com>
2026-05-26 21:48:28 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"BackgroundPanel"},{"p":"<Unnamed>","l":"GamePanel.FadingImagePanel"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"GamePanel"},{"p":"<Unnamed>","l":"GameWindow"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
